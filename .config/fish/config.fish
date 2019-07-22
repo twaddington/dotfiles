655c7e55ffd246a461e2bb7ce9ba5e0f0a795e63
Tristan Waddington <twaddington@polysync.io>
2019-07-22 14:36:58 -0700

    Add GOPATH to config.fish; fix exports

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
-set -x EDITOR vim
-set -x PATH $PATH $HOME/bin
+set -gx EDITOR vim
+set -gx GOPATH $HOME/.go
 
-# Use universal variables to configure this machine's environment.
+# universal variables, define machine specific settings.
 #
 # set -U JAVA_HOME (/usr/libexec/java_home -v 1.8)
 # set -U ANDROID_HOME $HOME/Library/Android/sdk
-
-# Use 'fish_user_paths' to set machine specific user paths.
+#
+# fish_user_paths, an array of directories that are prepended to PATH.
 #
 # set -U fish_user_paths $ANDROID_HOME/tools $fish_user_paths 
 # set -U fish_user_paths $ANDROID_HOME/platform-tools $fish_user_paths 
