e5db3677b66c27e467c6ff80ece0c19118d17b63
Tristan Waddington <tristan.waddington@gmail.com>
2020-01-13 09:21:19 -0800

    Tweak comments in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,15 @@ set -gx GOPATH $HOME/go
 
 # universal variables, define machine specific settings.
 #
-# set -U JAVA_HOME (/usr/libexec/java_home -v 1.8)
-# set -U ANDROID_SDK_ROOT $HOME/Library/Android/sdk
+# set -Ux JAVA_HOME (/usr/libexec/java_home -v 1.8)
+# set -Ux ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 #
 # fish_user_paths, an array of directories that are prepended to PATH.
 #
-# set -U fish_user_paths $ANDROID_SDK_ROOT/tools $fish_user_paths 
-# set -U fish_user_paths $ANDROID_SDK_ROOT/platform-tools $fish_user_paths 
-# set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths 
-# set -U fish_user_paths /usr/local/sbin $fish_user_paths 
+# set -Ua fish_user_paths $ANDROID_SDK_ROOT/tools
+# set -Ua fish_user_paths $ANDROID_SDK_ROOT/platform-tools
+# set -Ua fish_user_paths $HOME/.cargo/bin
+# set -Ua fish_user_paths /usr/local/sbin
 
 # THEME PURE #
 set fish_function_path /Users/tristan/.config/fish/functions/theme-pure/functions/ $fish_function_path
