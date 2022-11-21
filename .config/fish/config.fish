842d5a6bf55e8b6f8384d1455bb0f5b3e47bf629
Tristan Waddington <tristan.waddington@gmail.com>
2022-11-20 18:57:34 -0800

    Fix fish.config for updating path.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,16 +7,17 @@ set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 # set -Ux JAVA_HOME (/usr/libexec/java_home -v 1.8)
 # set -Ux ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 # set -Ux VOLTA_HOME $HOME/.volta
+# set -Ux CARGO_HOME $HOME/.cargo
 
-# fish_user_paths, an array of directories that are prepended to PATH.
+# how to update your $PATH
 #
-# set -a fish_user_paths $ANDROID_SDK_ROOT/tools
-# set -a fish_user_paths $ANDROID_SDK_ROOT/tools/bin
-# set -a fish_user_paths $ANDROID_SDK_ROOT/platform-tools
-# set -a fish_user_paths $HOME/.cargo/bin
-# set -a fish_user_paths /opt/homebrew/bin/
-# set -a fish_user_paths /usr/local/sbin
-# set -a fish_user_paths $VOLTA_HOME/bin
+# fish_add_path -v /opt/homebrew/bin/
+# fish_add_path -v /usr/local/sbin
+# fish_add_path -v $VOLTA_HOME/bin
+# fish_add_path -v $CARGO_HOME/bin
+# fish_add_path -v $ANDROID_SDK_ROOT/tools
+# fish_add_path -v $ANDROID_SDK_ROOT/tools/bin
+# fish_add_path -v $ANDROID_SDK_ROOT/platform-tools
 
 # THEME PURE #
 set fish_function_path /Users/tristan/.config/fish/functions/theme-pure/functions/ $fish_function_path
