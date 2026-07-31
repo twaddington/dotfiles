c042be8b3b427fe9a2e43545fc93b8317e2b433d
Tristan Waddington <tristan.waddington@gmail.com>
2026-07-31 15:58:21 -0700

    Add config.ghostty

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -138,7 +138,7 @@ print_success() {
 
 
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK bin .vim .config/fish"
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK bin .vim .config/fish .config/ghostty"
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
